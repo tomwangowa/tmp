de2b7340fb3e9ddfd49769cf678d80a31cdfde5c
tomwang.owa <tomwang.owa@gmail.com>
2020-01-15 22:29:24 +0800

    Check-in @ home (working code)
    Need to modify engine source code!!!

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,9 +1,35 @@
 import engineV3 from '../asset/engineV3';
 
 var root = {};
-const engineProbe = engineV3().probe;
-const engineModules = engineV3().modules;
 
-eval(engineProbe);
+// modules
+var $signupPattern$ = "";
+var $l10n$ = "";
+let engineModules = engineV3().modules;
+// pattern
+var $externalcss$ = "";
+let enginePattern = engineV3().pattern;
+// pattern1
+var $operationPattern$ = "{}";
+let enginePattern1 = engineV3().pattern1;
+// probe
+let engineProbe = engineV3().probe;
+
+// Uncaught SyntaxError: Delete of an unqualified identifier in strict mode.
+//=> In function:
+//=> eraseCachedObject: function(cachedObject) {
+//=>    cachedObject = null;
+//=>    delete cachedObject
+//=> }
+// So I comment out delete cachedObject in engineV3.
+
+// Also comment out 'this._sandbox.callback'
+
+//// ===> I think we need to modify engine code to meet "strict" mode policy.
+
 eval(engineModules);
+eval(enginePattern);
+eval(enginePattern1);
+eval(engineProbe);
+
 
